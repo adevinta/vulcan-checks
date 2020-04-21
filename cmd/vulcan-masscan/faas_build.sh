4184026046caf03c8ffa582a15a0b983ec67adab
Jesus-Carpintero <jesus.carpintero@adevinta.com>
2020-04-21 11:11:15 +0200

    Add nmap build options and faas_*.sh custom build scripts

---
+++ cmd/vulcan-masscan/faas_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apk update && apk add \
+	git \
+	gcc \
+	make \
+	libpcap-dev \
+	libc-dev \
+	linux-headers \
+	&& rm -rf /var/cache/apk/*
+
+git clone https://github.com/robertdavidgraham/masscan && \
+    cd ./masscan && \
+    make -j && \
+    cp -av ./bin/masscan /usr/local/bin
---
+++ cmd/vulcan-seekret/faas_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mv ./rules /opt
