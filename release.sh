012bbe9b3318ce634f84684072b3c5048aed3e39
Daniel Beneyto <danfaizer@gmail.com>
2020-05-13 12:16:31 +0200

    Modify version tag and add tags to output

--- release.sh
+++ release.sh
@@ -11,12 +11,13 @@ FORCE_BUILD="${FORCE_BUILD:-false}"
 # Force push even if conditions are not meet
 # Force does not override EXCLUDE_LIST_FILE
 FORCE_PUSH="${FORCE_PUSH:-false}"
+# Skip push even if conditions are meet
+# Force push prevails over Skip push
+SKIP_PUSH="${SKIP_PUSH:-false}"
 # Add time metrics to output
 PRINT_METRICS="${PRINT_METRICS:-true}"
 # Checks to exclude from build and push
 EXCLUDE_LIST_FILE="${EXCLUDE_LIST_FILE:-_scripts/exclude.lst}"
-# Sleep 1 second between registry calls
-REGISTRY_RATE_LIMIT="${REGISTRY_RATE_LIMIT:-true}"
 BASE_PATH="$PWD"
 
 # Load Libraries
@@ -46,6 +47,9 @@ go mod download
 for cf in cmd/*; do
     do_build=true
     do_push=true
+    if [[ $SKIP_PUSH == true ]]; then
+        do_push=false
+    fi
 
     ts_start=$(date +"%s")
     # Verify only directories are processed
@@ -62,35 +66,35 @@ for cf in cmd/*; do
     check_version=$(git_commit_id "$cf")
     check_timestamp=$(git_timestamp "$cf")
 
-    # Check if check has been pushed due to code or dependency update
-    pushed_by_dependency=$(dkr_image_exists "$check" "dep-$dep_version")
-    if [[ $REGISTRY_RATE_LIMIT == true ]]; then sleep 1; fi
-    pushed_by_check_code=$(dkr_image_exists "$check" "$check_version")
+    # Check if check version (code+dep) has been already pushed to Registry
+    already_pushed=$(dkr_image_exists "$check" "$check_version-$dep_version")
 
     # Check has been already pushed to Registry
-    if [[ $pushed_by_dependency == true && $pushed_by_check_code == true ]]; then
+    if [[ $already_pushed == true ]]; then
         do_build=$FORCE_BUILD
+        # Force push prevails over Skip push
         do_push=$FORCE_PUSH
         if [[ $do_build == false && $do_push == false ]]; then
-            echo "Skip build and push for check: [$check] - ALREADY PUSHED"
+            echo "Skip build and push for check: [$check] - ALREADY PUSHED - Version: [$check:$check_version-$dep_version]"
             continue
         fi
-        echo "Check: [$check] - ALREADY PUSHED - FORCE PUSH: $FORCE_PUSH"
+        echo "Check: [$check:$check_version-$dep_version] - ALREADY PUSHED - FORCE PUSH: $FORCE_PUSH"
     fi
 
     # Check if the image is available locally
-    local_by_dependency=$(dkr_local_image_exists "$check" "dep-$dep_version")
-    local_by_check_code=$(dkr_local_image_exists "$check" "$check_version")
+    local_exists=$(dkr_local_image_exists "$check" "$check_version-$dep_version")
 
     # If exists locally, we should build only if forced
-    if [[ $local_by_dependency == true && $local_by_check_code == true ]]; then
+    if [[ $local_exists == true ]]; then
         if [[ ! $FORCE_BUILD == true ]]; then
             do_build=false
         fi
     fi
 
-    echo "Processing: [$check] | ID: $check_version TS: $check_timestamp DEP_ID: $dep_version MODE: $check_mode"
-    # Build go binaries
+    echo "Processing: [$check] | ID: $check_version-$dep_version TS: $check_timestamp MODE: $check_mode"
+    # List of tags to apply to check Docker image
+    tag_list="latest,$check_version-$dep_version,$check_timestamp,dep-$dep_timestamp,$check_branch,$check_mode"
+    # Build check (Go binaries and Docker images + Tagging)
     if [[ $do_build == true ]]; then
         cd "$cf" || exit 1
         ts_go_build_start=$(date +"%s")
@@ -105,7 +109,7 @@ for cf in cmd/*; do
 
         # Tag docker image
         ts_docker_tag_start=$(date +"%s")
-        dkr_tag "$check" "latest,$check_version,$check_timestamp,dep-$dep_version,dep-$dep_timestamp,$check_branch,$check_mode"
+        dkr_tag "$check" "$tag_list"
         ts_docker_tag_finish=$(date +"%s")
     else
         echo "Skip build process for check: [$check]"
@@ -134,6 +138,7 @@ for cf in cmd/*; do
         docker_tag=$((ts_docker_tag_finish-ts_docker_tag_start))
         docker_push=$((ts_docker_push_finish-ts_docker_push_start))
         echo "## Build and push stats for check [$check]"
+        echo "## Tags available: [$tag_list]"
         echo "## $total_process seconds : total process"
         echo "## $go_build seconds : go build process"
         echo "## $docker_build seconds : docker build process"
