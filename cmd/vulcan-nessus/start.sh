9561cd2d904af038eb9d80342d1c03785caab579
Daniel Beneyto <danfaizer@gmail.com>
2021-11-23 10:36:30 +0100

    Start nessus agent if LINKING_KEY is provided

--- cmd/vulcan-nessus/start.sh
+++ cmd/vulcan-nessus/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-supervisord -c /etc/supervisord.conf &
+# If LINKING_KEY is not provided do not start
+# Nessus agent in the local container.
+if [ -n "$LINKING_KEY" ]; then
+    supervisord -c /etc/supervisord.conf &
+fi
 ./vulcan-nessus
