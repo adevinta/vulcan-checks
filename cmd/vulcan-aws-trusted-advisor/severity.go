669005fda597da78858885dcc0fbfad7c5d25c76
Redsadic <julian.vilas@gmail.com>
2022-01-26 09:16:38 +0100

    Expand aws trusted advisor (#138)

--- cmd/vulcan-aws-trusted-advisor/severity.go
+++ cmd/vulcan-aws-trusted-advisor/severity.go
@@ -5,7 +5,7 @@ Copyright 2019 Adevinta
 package main
 
 import (
-	"github.com/adevinta/vulcan-report"
+	report "github.com/adevinta/vulcan-report"
 )
 
 // Information extracted from: https://aws.amazon.com/premiumsupport/ta-iam/
@@ -13,98 +13,98 @@ var severityMap = map[string]map[string]float32{
 
 	// Security Groups - Unrestricted Access
 	"1iG5NDGVre": map[string]float32{
-		"red": report.SeverityThresholdMedium,
+		"Red": report.SeverityThresholdMedium,
 	},
 
 	// Amazon S3 Bucket Permissions
 	"Pfx0RwqBli": map[string]float32{
-		"yellow": report.SeverityThresholdMedium,
-		"red":    report.SeverityThresholdHigh,
+		"Yellow": report.SeverityThresholdMedium,
+		"Red":    report.SeverityThresholdHigh,
 	},
 
 	// ELB Listener Security
 	"a2sEc6ILx": map[string]float32{
-		"yellow": report.SeverityThresholdMedium,
-		"red":    report.SeverityThresholdHigh,
+		"Yellow": report.SeverityThresholdMedium,
+		"Red":    report.SeverityThresholdHigh,
 	},
 
 	// ELB Security Groups
 	"xSqX82fQu": map[string]float32{
-		"yellow": report.SeverityThresholdNone,
-		"red":    report.SeverityThresholdNone,
+		"Yellow": report.SeverityThresholdNone,
+		"Red":    report.SeverityThresholdNone,
 	},
 
 	// IAM Access Key Rotation
 	"DqdJqYeRm5": map[string]float32{
-		"green":  report.SeverityThresholdNone,
-		"yellow": report.SeverityThresholdLow,
-		"red":    report.SeverityThresholdMedium,
+		"Green":  report.SeverityThresholdNone,
+		"Yellow": report.SeverityThresholdLow,
+		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// Security Groups - Specific Ports Unrestricted
 	"HCP4007jGY": map[string]float32{
-		"green":  report.SeverityThresholdNone,
-		"yellow": report.SeverityThresholdLow,
-		"red":    report.SeverityThresholdMedium,
+		"Green":  report.SeverityThresholdNone,
+		"Yellow": report.SeverityThresholdLow,
+		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// Amazon EBS Public Snapshots
 	"ePs02jT06w": map[string]float32{
-		"red": report.SeverityThresholdMedium,
+		"Red": report.SeverityThresholdMedium,
 	},
 
 	// Amazon RDS Public Snapshots
 	"rSs93HQwa1": map[string]float32{
-		"red": report.SeverityThresholdHigh,
+		"Red": report.SeverityThresholdHigh,
 	},
 
 	// Amazon RDS Security Group Access Risk
 	"nNauJisYIT": map[string]float32{
-		"yellow": report.SeverityThresholdLow,
-		"red":    report.SeverityThresholdMedium,
+		"Yellow": report.SeverityThresholdLow,
+		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// Amazon Route 53 MX and SPF Resource Record Sets
 	"c9D319e7sG": map[string]float32{
-		"yellow": report.SeverityThresholdMedium,
+		"Yellow": report.SeverityThresholdMedium,
 	},
 
 	// AWS CloudTrail Logging
 	"vjafUGJ9H0": map[string]float32{
-		"yellow": report.SeverityThresholdLow,
-		"red":    report.SeverityThresholdLow,
+		"Yellow": report.SeverityThresholdLow,
+		"Red":    report.SeverityThresholdLow,
 	},
 
 	// CloudFront Custom SSL Certificates in the IAM Certificate Store
 	"N425c450f2": map[string]float32{
-		"yellow": report.SeverityThresholdMedium,
-		"red":    report.SeverityThresholdMedium,
+		"Yellow": report.SeverityThresholdMedium,
+		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// CloudFront SSL Certificate on the Origin Server
 	"N430c450f2": map[string]float32{
-		"yellow": report.SeverityThresholdMedium,
-		"red":    report.SeverityThresholdMedium,
+		"Yellow": report.SeverityThresholdMedium,
+		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// Exposed Access Keys
 	"12Fnkpl8Y5": map[string]float32{
-		"red": report.SeverityThresholdCritical,
+		"Red": report.SeverityThresholdCritical,
 	},
 
 	// IAM Password Policy
 	"Yw2K9puPzl": map[string]float32{
-		"yellow": report.SeverityThresholdNone,
-		"red":    report.SeverityThresholdNone,
+		"Yellow": report.SeverityThresholdNone,
+		"Red":    report.SeverityThresholdNone,
 	},
 
 	// IAM Use
 	"zXCkfM1nI3": map[string]float32{
-		"yellow": report.SeverityThresholdNone,
+		"Yellow": report.SeverityThresholdNone,
 	},
 
 	// MFA on Root Account
 	"7DAFEmoDos": map[string]float32{
-		"red": report.SeverityThresholdLow,
+		"Red": report.SeverityThresholdLow,
 	},
 }
