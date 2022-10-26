5e49a442c24afafc0ce7e2d09db64dc76c61ebf5
Jesus Carpintero <jesus.carpintero@adevinta.com>
2022-10-26 16:05:08 +0200

    Move check-catalog process to a script

---
+++ release-check-catalog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copyright 2022 Adevinta
+
+mkdir pages/checktypes
+
+go install "github.com/adevinta/vulcan-check-catalog/cmd/vulcan-check-catalog@${VCC_VERSION:-main}"
+
+if [[ $TRAVIS_BRANCH == "master" ]]; then
+    vulcan-check-catalog -registry-url "$DKR_USERNAME" -tag edge -output pages/checktypes/edge.json cmd/
+else
+    echo "Skipping because not in master branch"
+fi
