fbf37cf6b2e0e7938a774185df2fe08e45b4985e
Jesus Carpintero <jesus.carpintero@adevinta.com>
2023-02-16 14:11:16 +0100

    move to script

---
+++ _scripts/setup_buildx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if docker buildx version; then
+    exit 0
+fi
+
+sudo mkdir -m 0755 -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
+sudo apt-get update
+sudo apt-get install docker-buildx-plugin
+
+docker buildx version
