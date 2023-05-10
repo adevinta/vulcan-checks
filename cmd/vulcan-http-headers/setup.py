16eae067746cdd7e263da4cd3f0eef2d5939f9f8
Jesús Carpintero <jesus.carpintero@adevinta.com>
2023-05-10 17:02:43 +0200

    remove vulcan-http-headers (#299)

--- cmd/vulcan-http-headers/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from setuptools import setup, find_packages
-setup(
-    name='httpobs',
-    packages=find_packages(),
-    include_package_data=True,
-    scripts=['httpobs/scripts/httpobs-local-scan'],
-)
