90fd84ebd6b76649791c4e0430d825c6b7495318
Julian Vilas <julian.vilas@gmail.com>
2023-09-20 10:07:28 +0200

    vulcan-trivy.go: remove backticks in comments

--- cmd/vulcan-trivy/vulcan-trivy.go
+++ cmd/vulcan-trivy/vulcan-trivy.go
@@ -34,8 +34,8 @@ const (
 	vulnCVETruncateLimit = 10
 	DefaultDepth         = 1
 	// trivyTimeout defines the value that will be passed to the trivy CLI via
-	// the `--timeout` flag. The value should be bigger than the check timeout
-	// defined in the manifest, to ensure the check will have a `TIMEOUT`
+	// the '--timeout' flag. The value should be bigger than the check timeout
+	// defined in the manifest, to ensure the check will have a 'TIMEOUT'
 	// status when the execution takes longer than expected.
 	trivyTimeout = "2h"
 )
