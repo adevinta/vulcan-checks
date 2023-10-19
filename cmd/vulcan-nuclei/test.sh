1d78f4164f41d6e0b994e53076c9500d3608e335
Daniel Beneyto <danfaizer@gmail.com>
2023-10-19 10:27:36 +0200

    Add template validation in vulcan-nuclei tests

--- cmd/vulcan-nuclei/test.sh
+++ cmd/vulcan-nuclei/test.sh
@@ -4,11 +4,15 @@ set -eu
 
 . _scripts/libtest.sh
 
+# Validate custom templates.
+docker run --rm -v ./cmd/vulcan-nuclei/templates:/tmp/templates projectdiscovery/nuclei -duc -validate -ud /tmp/templates
 
-CONTAINERID=$(docker run -p 21:21 -d garethflowers/ftp-server:0.6.0)
+CONTAINERID1=$(docker run -p 21:21 -d garethflowers/ftp-server:0.6.0)
+CONTAINERID2=$(docker run -p 6379:6379 -d redis:7.2-alpine)
 trap finish EXIT
 function finish {
-    docker rm -f "$CONTAINERID" || true
+    docker rm -f "$CONTAINERID1" || true
+    docker rm -f "$CONTAINERID2" || true
 }
 
-vulcan_local_test -i "$1" -t localhost -a Hostname -o '{"tag_inclusion_list":["ftp"]}'
+vulcan_local_test -i "$1" -t localhost -a Hostname -o '{"tag_inclusion_list":["ftp", "detect"]}'
