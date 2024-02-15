222284c35d67098fd43754fa9c6fbaf64a96b21f
Manel Montilla <manel.montilla@gmail.com>
2024-02-15 14:48:20 +0100

    vulcan-aws-trusted-advisor/severity.go: fix style issues

--- cmd/vulcan-aws-trusted-advisor/severity.go
+++ cmd/vulcan-aws-trusted-advisor/severity.go
@@ -12,99 +12,99 @@ import (
 var severityMap = map[string]map[string]float32{
 
 	// Security Groups - Unrestricted Access
-	"1iG5NDGVre": map[string]float32{
+	"1iG5NDGVre": {
 		"Red": report.SeverityThresholdMedium,
 	},
 
 	// Amazon S3 Bucket Permissions
-	"Pfx0RwqBli": map[string]float32{
+	"Pfx0RwqBli": {
 		"Yellow": report.SeverityThresholdMedium,
 		"Red":    report.SeverityThresholdHigh,
 	},
 
 	// ELB Listener Security
-	"a2sEc6ILx": map[string]float32{
+	"a2sEc6ILx": {
 		"Yellow": report.SeverityThresholdMedium,
 		"Red":    report.SeverityThresholdHigh,
 	},
 
 	// ELB Security Groups
-	"xSqX82fQu": map[string]float32{
+	"xSqX82fQu": {
 		"Yellow": report.SeverityThresholdNone,
 		"Red":    report.SeverityThresholdNone,
 	},
 
 	// IAM Access Key Rotation
-	"DqdJqYeRm5": map[string]float32{
+	"DqdJqYeRm5": {
 		"Green":  report.SeverityThresholdNone,
 		"Yellow": report.SeverityThresholdLow,
 		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// Security Groups - Specific Ports Unrestricted
-	"HCP4007jGY": map[string]float32{
+	"HCP4007jGY": {
 		"Green":  report.SeverityThresholdNone,
 		"Yellow": report.SeverityThresholdLow,
 		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// Amazon EBS Public Snapshots
-	"ePs02jT06w": map[string]float32{
+	"ePs02jT06w": {
 		"Red": report.SeverityThresholdMedium,
 	},
 
 	// Amazon RDS Public Snapshots
-	"rSs93HQwa1": map[string]float32{
+	"rSs93HQwa1": {
 		"Red": report.SeverityThresholdHigh,
 	},
 
 	// Amazon RDS Security Group Access Risk
-	"nNauJisYIT": map[string]float32{
+	"nNauJisYIT": {
 		"Yellow": report.SeverityThresholdLow,
 		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// Amazon Route 53 MX and SPF Resource Record Sets
-	"c9D319e7sG": map[string]float32{
+	"c9D319e7sG": {
 		"Yellow": report.SeverityThresholdMedium,
 	},
 
 	// AWS CloudTrail Logging
-	"vjafUGJ9H0": map[string]float32{
+	"vjafUGJ9H0": {
 		"Yellow": report.SeverityThresholdLow,
 		"Red":    report.SeverityThresholdLow,
 	},
 
 	// CloudFront Custom SSL Certificates in the IAM Certificate Store
-	"N425c450f2": map[string]float32{
+	"N425c450f2": {
 		"Yellow": report.SeverityThresholdMedium,
 		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// CloudFront SSL Certificate on the Origin Server
-	"N430c450f2": map[string]float32{
+	"N430c450f2": {
 		"Yellow": report.SeverityThresholdMedium,
 		"Red":    report.SeverityThresholdMedium,
 	},
 
 	// Exposed Access Keys
-	"12Fnkpl8Y5": map[string]float32{
+	"12Fnkpl8Y5": {
 		"Red": report.SeverityThresholdCritical,
 	},
 
 	// IAM Password Policy
-	"Yw2K9puPzl": map[string]float32{
+	"Yw2K9puPzl": {
 		"Yellow": report.SeverityThresholdNone,
 		"Red":    report.SeverityThresholdNone,
 	},
 
 	// IAM Use
-	"zXCkfM1nI3": map[string]float32{
+	"zXCkfM1nI3": {
 		"Yellow": report.SeverityThresholdNone,
 	},
 
 	// MFA on Root Account
-	"7DAFEmoDos": map[string]float32{
+	"7DAFEmoDos": {
 		"Red": report.SeverityThresholdLow,
 	},
 }
